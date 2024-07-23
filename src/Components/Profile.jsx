ebb37bfcca0508c61fe0aee7cb7845c42103b68c
Nuuradiin-zubeyr <nuurosman2002@gmail.com>
2024-07-23 12:10:38 -0700

    Profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,6 +1,6 @@
 function Profile({name, bio, bgColor, image}) {
 
-    return <div className="border-2 border-black py-3 px-6 w-[500px] mx-10 my-10">
+    return <div className="border-2 border-black py-3 px-6 sm:w-[500px] w-[388px]  sm:mx-10 my-10">
         <img className="w-[500px] h-[300px]" src= {image} />
         <h1 className="text-2xl font-semibold"> {name} </h1>
         <div className="flex gap-2  py-2 text-center">
