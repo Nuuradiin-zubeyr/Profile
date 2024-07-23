821060834328beb35f4c8ca8e71db37128481413
Nuuradiin-zubeyr <nuurosman2002@gmail.com>
2024-07-23 11:57:07 -0700

    Profile

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+function Profile({name, bio, bgColor, image}) {
+
+    return <div className="border-2 border-black py-3 px-6 w-[500px] mx-10 my-10">
+        <img className="w-[500px] h-[300px]" src= {image} />
+        <h1 className="text-2xl font-semibold"> {name} </h1>
+        <div className="flex gap-2  py-2 text-center">
+            <div className="w-[50px] h-[30px] text-white rounded bg-teal-400 ">
+                <h1>Html</h1>
+
+            </div>
+            <div className="w-[50px] h-[30px] text-white rounded bg-teal-400 ">
+                <h1>Css</h1>
+
+            </div>
+            <div className="w-[50px] h-[30px] text-white rounded bg-teal-400 ">
+                <h1>Js</h1>
+
+            </div>
+
+        </div>
+        <p>
+            {bio}
+        </p>
+        <button style={{backgroundColor:bgColor}} className="bg-stone-500 px-10 py-2 text-white rounded w-[300px] my-4">Hire Me</button>
+
+    </div>
+}
+export default Profile
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Components/Profile"
+import ReactDOM from "react-dom/client"
+import "./Components/Style.css";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className="flex justify-center">
+    <Profile name=  "Ali ahmed" bgColor="blue" bio= "Hello I am Ali iam softwere developer i have these skills "  image="https://i.pinimg.com/236x/e6/33/ee/e633eefbeb77cd4323a1557d33c91c83.jpg" />
+    <Profile name= "Nuradiin Osman" bgColor= "red" bio= "Hello Sir iam Nuradiin Osman FullStack Softwere Eng and i have this Down Skills and Their Exprience" image="https://i.pinimg.com/564x/47/91/f0/4791f027dcad85f85883359daf191c5d.jpg" />
+    <Profile name= "Abdi Nasir" bgColor= "orange" bio= "Hi All iam Abdi Nasir i am Senior Mobile App developer and i have this skills" image="https://i.pinimg.com/564x/6e/a9/d3/6ea9d3a86db7a28dce962115aaf815db.jpg" />
+  </div>
+
+)
